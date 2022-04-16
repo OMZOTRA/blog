b5d8d5534fe82899b4c97e495aa2243c6ab9e610
OMZOTRA <toumartra@gmail.com>
2022-04-16 18:47:52 +0200

    version2.1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import "./assets/styles/styles.scss";
 import "./index.scss";
 
-const articleContainerElement=document.querySelector(".articles-container");
+const articleContainerElement = document.querySelector(".articles-container");
+const categoriesContainerElement = document.querySelector(".categories");
+let filter;
+let articles;
 
-const createArticles = articles => {
-  const articlesDOM = articles.map(article => {
+// la Methode filter permet de litrer les article par categorie et ensuite de les affichers lorsque la 
+// la categorie existe
+const createArticles = () => {
+  const articlesDOM = articles.filter(article => {
+    if(filter){
+       return article.category === filter;
+    }else{
+      return true;
+    }
+  }).map(article => {
     const articleDOM = document.createElement("div");
     articleDOM.classList.add("article");
     articleDOM.innerHTML = `
@@ -12,20 +23,36 @@ const createArticles = articles => {
   src="${article.img}"
   alt="profile"
 />
-<h2>${article.title}</h2>
-<p class="article-author">${article.author} - ${article.category}</p>
+<h2>${article.title}</h2>                    
+<p class="article-author">${article.author} - ${new Date(article.createdAt).toLocaleDateString("fr-FR",{
+  weekday : 'long',  
+  day : '2-digit',
+  month : 'long',
+  year : "numeric"
+})}</p>
 <p class="article-content">
   ${article.content}
 </p>
 <div class="article-actions">
   <button class="btn btn-danger" data-id=${article._id} >Supprimer</button>
+  <button class="btn btn-primary" data-id=${article._id} >Modifier</button>
 </div>
 `;
 return articleDOM;
   });
-  articleContainerElement.innerHTML = '';
+  articleContainerElement.innerHTML = "";
   articleContainerElement.append(...articlesDOM);
   const deleteButtons = articleContainerElement.querySelectorAll(".btn-danger");
+  const editButton = articleContainerElement.querySelectorAll(".btn-primary");
+
+  // pour basculer en mode edition quand ton click sur la button modifier
+  editButton.forEach(button => {
+    button.addEventListener("click", event => {
+       const target = event.target; // pour recuperer la cible 
+       const articleId = target.dataset.id; // pour récupérer Id de l'article cibler
+       location.assign(`/form.html?id=${articleId}`); // une redirection vers article cibler 
+    });
+  });
   deleteButtons.forEach(button => {
     button.addEventListener("click", async event => {
         try {
@@ -46,14 +73,59 @@ return articleDOM;
     });
   });
 };
+// permet d'afficher le Menu categories
+const displayMenuCategories = (categoriesArr) => {
+  const liElements = categoriesArr.map(categoryElem => {
+    const li = document.createElement("li");
+    li.innerHTML = `<li>${categoryElem[0]}(<strong>${categoryElem[1]}</strong>)</li>`;
+    li.addEventListener("click", () => {
+      if(filter === categoryElem[0]){ //Nous permet de désélection la category déja séléctionner                          
+        filter = null;                //En mettant le filter à null et ensuite de supprimer la classe active 
+        li.classList.remove('active');// par remove('active')
+        createArticles();
+      }else{
+        filter = categoryElem[0];
+        liElements.forEach(li => { // "forEach" permet de parcourir chaque élement Li 
+          li.classList.remove('active');
+        })
+        li.classList.add("active");
+        createArticles();
+      } 
+    })
+    return li;
+  });
+  categoriesContainerElement.innerHTML="";
+  categoriesContainerElement.append(...liElements);
+}
+
+//La fonction createMenuCategories() va prendre les articles récupérées du serveur et va commencer par les
+// transformer en objet avec la méthode reduce() que nous avions vue.
+const createMenuCategories = () =>{
+  const categories = articles.reduce((acc,article) => {
+     if (acc[article.category]){
+         acc[article.category] ++;
+      } else{
+        acc[article.category] = 1;
+      }
+      return acc ;
+  },{});
+  // creation dynamique du Menu Catégories
+  const categoriesArr = Object.keys(categories).map((category) =>{
+    return [category, categories[category]];
+  })
+  .sort((c1,c2) => c1[0].localeCompare(c2[0]));//la mehtode sort()permet de faire le trie par 
+                  //lettre Alphabetique et le methode LocaleCompare() d'éffectuer une comparaison locale
+  displayMenuCategories(categoriesArr); // permet d'afficher le Menu categories 
+}
 
 const fetchArticle = async () => {
     try {
       const response = await fetch("https://restapi.fr/api/article");
-      const articles = await response.json();
-      createArticles(articles);
+      articles = await response.json();
+      createArticles();
+      createMenuCategories();
     } catch (e) {
-      console.log("e : ", e);
+      console.log("e :", e);
     }
   };
   
